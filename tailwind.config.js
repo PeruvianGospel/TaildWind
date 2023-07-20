8bb4939f893aa420cf0a2054cb55eeaf6c2e5573
PeruvianGospel <theperuviangospel@gmail.com>
2023-07-20 16:45:58 -0500

    primer comit git bash

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+  },
+  extend: {
+      colors:{
+        heiler:'#F4F2DE'
+      },
+  },
+  plugins: [],
+}
+
